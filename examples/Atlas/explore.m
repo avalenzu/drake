26312394d0b1dc89f860f131439d2ff234114ba0
Andrés Valenzuela <avalenzu@mit.edu>
2014-01-27 09:04:31 -0500

    Add feed-forward torque in `explore.m`
    This allows me to lower the controller gains, which in turn lets me
    increase the simulation time-step. As a result, the whole thing runs 10x
    faster.

--- examples/Atlas/explore.m
+++ examples/Atlas/explore.m
@@ -9,7 +9,7 @@ foot = length(rbm.body);
 foot_frame = RigidBodyFrame(foot,zeros(3,1),zeros(3,1),'foot_origin');
 rbm = addFrame(rbm,foot_frame);
 rbm = rbm.compile();
-tsrbm = TimeSteppingRigidBodyManipulator(rbm,0.001);
+tsrbm = TimeSteppingRigidBodyManipulator(rbm,0.01);
 nq = rbm.getNumDOF();
 nu = rbm.getNumInputs();
 % encoders = RigidBodyJointSensor(tsrbm.getManipulator());
@@ -22,15 +22,15 @@ tsrbm = compile(tsrbm);
 nx = tsrbm.getOutputFrame().getFrameByNum(1).dim;
 nf = tsrbm.getOutputFrame().getFrameByNum(2).dim;
 %%
-% v = tsrbm.constructVisualizer();
+ v = tsrbm.constructVisualizer();
 % v.debug = true;
-foot_pts = rbm.body(foot).getContactPoints();
-qsc = QuasiStaticConstraint(rbm);
-qsc = qsc.addContact(foot,foot_pts);
-qsc = qsc.setActive(true);
-qsc = qsc.setShrinkFactor(0.7);
-kc_foot = WorldPositionConstraint(rbm,foot,foot_pts,[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))],[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))]);
-above_ground_kc = WorldCoMConstraint(rbm,[0;0;0],[0;Inf;Inf]);
+%foot_pts = rbm.body(foot).getContactPoints();
+%qsc = QuasiStaticConstraint(rbm);
+%qsc = qsc.addContact(foot,foot_pts);
+%qsc = qsc.setActive(true);
+%qsc = qsc.setShrinkFactor(0.7);
+%kc_foot = WorldPositionConstraint(rbm,foot,foot_pts,[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))],[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))]);
+%above_ground_kc = WorldCoMConstraint(rbm,[0;0;0],[0;Inf;Inf]);
 %%
 [joint_limits_min,joint_limits_max] = rbm.getJointLimits();
 % joint_limits_min(4:6) = -pi/10;
@@ -42,10 +42,10 @@ cost.base_z = 0;
 cost.base_relative_pitch = 0;
 ikoptions = ikoptions.setQ(diag(cost(1:nq)));
 %%
-Kp = diag([5e3,5e3,1e4,1e4]);
-Kd = 1e-1*eye(nu);
+Kp = diag([1e2,1e2,1e3,1e3]);
+Kd = 3e-2*Kp;
 %%
-sys = tsrbm.pdcontrol(Kp,Kd);
+[pdff,pdfb] = tsrbm.pdcontrol(Kp,Kd);
 %%
 n = 1000;
 t_data = zeros(1,n);
@@ -53,47 +53,60 @@ q_data = zeros(nq,n);
 ft_data = zeros(3,n);
 x_traj_data = cell(1,n);
 i = 1;
-while i < n
-  tic;
-  fprintf('Iteration %d\n',i);
-  q_nom = Point(rbm.getStateFrame(), ...
-                [rand(nq,1).*(joint_limits_max - joint_limits_min) + joint_limits_min; ...
-                zeros(nq,1)]);
-  q_nom.base_x = 0;
-  q_nom.base_z = 1;
-  q_nom.base_relative_pitch = 0;
-%     v.draw(0,q_nom);
-  
-  %[q,info] = inverseKin(rbm,q_nom,q_nom,qsc,kc_foot,above_ground_kc,ikoptions);
-  [q,info] = inverseKin(rbm,q_nom(1:nq),q_nom(1:nq),qsc,kc_foot,ikoptions);
-  x0 = inFrame(Point(rbm.getStateFrame(),[q;zeros(size(q))]),tsrbm.getStateFrame());
-  xstar = x0;
-  if info == 1
-    success = true;
-%     try
-%       [xstar,ustar,success] = findFixedPoint(tsrbm,x0,zeros(nu,1));
-%     catch ex
-%       success = false;
-%     end
-  else
-    success = false;
-  end
-  if success
-    sys_sim = cascade(ConstantTrajectory(Point(sys.getInputFrame,xstar(tsrbm.getActuatedJoints))),sys);
-    try
-      [x_traj,y_traj] = simulate(sys_sim,[0,5],[xstar]);
-%       x_traj_vis = PPTrajectory(foh(x_traj.tt,x_traj.xx));
-%       x_traj_vis = x_traj_vis.setOutputFrame(v.getInputFrame());
-%       v.playback(x_traj_vis)
-    catch ex
+parfor i = 1:n
+  pdff_i = pdff;
+  success = false;
+  while ~success
+    tic;
+    fprintf('Iteration %d\n',i);
+    q_nom = Point(rbm.getStateFrame(), ...
+      [rand(nq,1).*(joint_limits_max - joint_limits_min) + joint_limits_min; ...
+      zeros(nq,1)]);
+    q_nom.base_x = 0;
+    q_nom.base_z = 1;
+    q_nom.base_relative_pitch = 0;
+    %     v.draw(0,q_nom);
+
+    foot_pts = rbm.body(foot).getContactPoints();
+    qsc = QuasiStaticConstraint(rbm);
+    qsc = qsc.addContact(foot,foot_pts);
+    qsc = qsc.setActive(true);
+    qsc = qsc.setShrinkFactor(0.7);
+    kc_foot = WorldPositionConstraint(rbm,foot,foot_pts,[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))],[nan(2,size(foot_pts,2));zeros(1,size(foot_pts,2))]);
+    above_ground_kc = WorldCoMConstraint(rbm,[0;0;0],[0;Inf;Inf]);
+    %[q,info] = inverseKin(rbm,q_nom,q_nom,qsc,kc_foot,above_ground_kc,ikoptions);
+    [q,info] = inverseKin(rbm,q_nom(1:nq),q_nom(1:nq),qsc,kc_foot,ikoptions);
+    x0 = inFrame(Point(rbm.getStateFrame(),[q;zeros(size(q))]),tsrbm.getStateFrame());
+    xstar = x0;
+    if info == 1
+      %success = true;
+      try
+        [xstar,ustar,success] = findFixedPoint(tsrbm,x0,zeros(nu,1));
+      catch ex
+        success = false;
+      end
+    else
       success = false;
     end
-  end
-  if success
-    x_traj_data{i} = x_traj;
-  else
-    disp('Failed to find fixed point near this seed.');
-  end
-  save('staticData','x_traj_data');
-  toc;
-end;
+    if success
+      pdff_i.y0 = double(ustar);
+      sys = cascade(pdff_i,feedback(tsrbm,pdfb));
+      sys_sim = cascade(ConstantTrajectory(Point(sys.getInputFrame,xstar(tsrbm.getActuatedJoints))),sys);
+      try
+        [x_traj,y_traj] = simulate(sys_sim,[0,5],xstar);
+        %x_traj_vis = PPTrajectory(foh(x_traj.tt,x_traj.xx));
+        %x_traj_vis = x_traj_vis.setOutputFrame(v.getInputFrame());
+        %v.playback(x_traj_vis)
+      catch ex
+        success = false;
+      end
+    end
+    if success
+      x_traj_data{i} = x_traj;
+    else
+      disp('Failed to find fixed point near this seed.');
+    end
+    toc;
+  end;
+end
+save('staticData','x_traj_data');
