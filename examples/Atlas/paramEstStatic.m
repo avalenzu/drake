de10e503e265c7822489c0ceefe296ee65ef0623
avalenzu <avalenzu@mit.edu>
2014-01-31 14:25:47 -0500

    Split data into training and validation sets.

--- examples/Atlas/paramEstStatic.m
+++ examples/Atlas/paramEstStatic.m
@@ -53,26 +53,33 @@ p_guess = p_orig + 5e-1*(2*rand(np,1)-1).*(p_orig+1);
 lp_guess = double(subs(lp,p,p_guess));
 chi_guess = double(subs(chi,p,p_guess));
 %% Populate regression matrices
-n = size(q_data,2);
-s_data = sin(q_data);
-c_data = cos(q_data);
+idx_validation = rand(1,size(q_data,2)) > 0.05;
+idx_training = ~idx_validation;
+q_data_validation = q_data(:,idx_validation);
+q_data_training = q_data(:,idx_training);
+ft_data_validation = ft_data(:,idx_validation);
+ft_data_training = ft_data(:,idx_training);
+n = size(q_data_training,2);
+n_validation = sum(idx_validation);
+s_data = sin(q_data_training);
+c_data = cos(q_data_training);
 W_col = reshape(W',[],1);
-W_raw = full(msubs(getmsspoly(W_col),[q;s;c],[q_data;s_data;c_data])');
+W_raw = full(msubs(getmsspoly(W_col),[q;s;c],[q_data_training;s_data;c_data])');
 W_data = reshape(W_raw',nchi,[])';
 rho_col = reshape(rho',[],1);
-rho_data = full(msubs(getmsspoly(rho_col),[q;s;c],[q_data;s_data;c_data]));
+rho_data = full(msubs(getmsspoly(rho_col),[q;s;c],[q_data_training;s_data;c_data]));
 Y_data = zeros(size(W,1),n);
 for i = 1:n
-  kinsol = doKinematics(rbm,q_data(:,i));
+  kinsol = doKinematics(rbm,q_data_training(:,i));
   [pos_foot,J_foot] = forwardKin(rbm,kinsol,rbm.findFrameId(foot_frame.name),[0;0;0],1);
   J_foot([2,4,6],:) = [];J_foot = J_foot(:,rows_of_interest);
-  Y_data(:,i) = J_foot'*ft_data(:,i);
+  Y_data(:,i) = J_foot'*ft_data_training(:,i);
 end
 
 %% Estimate identifiable parameters
 %idx_good = (Y_data - W_data*chi_orig).^2 < 200;
 %idx_good = 1:n;
-idx_good = ft_data(2,:) > 0;
+idx_good = ft_data_training(2,:) > 0;
 Y_data = reshape(Y_data(:,idx_good),[],1);
 rho_data = reshape(rho_data(:,idx_good),[],1);
 chi_est = W_data(reshape(repmat(idx_good,size(W,1),1),[],1),:)\(Y_data-rho_data);
@@ -111,3 +118,29 @@ fprintf('  -----  \t------  \t--------\t---------\t-------------\t--------------
 for i=1:length(coords)
   fprintf('%7s  \t%8.2f\t%8.2f\t%8.2f\t%8.2f\t%8.2f\n',coords{i},p_orig(i),p_guess(i),phat(i),p_guess_error(i),p_error(i));
 end
+
+%% Validation
+rbm_validation = rbm.setParams(phat);
+rbm_guess = rbm.setParams(p_guess);
+com_guess = zeros(2,n_validation);
+com_actual = zeros(2,n_validation);
+com_validation = zeros(2,n_validation);
+for i = 1:n_validation
+  kinsol = doKinematics(rbm,q_data_validation(:,i));
+  com_actual(:,i) = rbm.getCOM(kinsol);
+  kinsol = doKinematics(rbm_guess,q_data_validation(:,i));
+  com_guess(:,i) = rbm_guess.getCOM(kinsol);
+  kinsol = doKinematics(rbm_validation,q_data_validation(:,i));
+  com_validation(:,i) = rbm_validation.getCOM(kinsol);
+end
+
+com_error_guess = sqrt(sum((com_actual - com_guess).^2,1));
+com_error_validation = sqrt(sum((com_actual - com_validation).^2,1));
+
+%%
+fprintf('\nValidation results: Errors in COM location\n\n');
+fprintf('                    Mean  \tStd. Dev \tMax\n');
+fprintf('                    ----- \t-------- \t---\n');
+fprintf('Initial Guess     | %8.2e\t%8.2e\t%8.2e\n',mean(com_error_guess),std(com_error_guess),max(com_error_guess));
+fprintf('Estimated Params. | %8.2e\t%8.2e\t%8.2e\n',mean(com_error_validation),std(com_error_validation),max(com_error_validation));
+
