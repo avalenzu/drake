46d921b2162dfbab088cdbbea1876310d61046c7
Hongkai Dai <daihongkai@gmail.com>
2014-04-24 21:55:36 -0400

    RelativeFOotPositionPolygon is implemented to encode the polygonal constraint on the foot location w.r.t the other foot

---
+++ systems/plants/trajOpt/test/testRelativeFootPositionPolygon.m
@@ -0,0 +1,18 @@
+function testRelativeFootPositionPolygon
+vertices = randn(2,7);
+rfpp = RelativeFootPositionPolygon(vertices);
+
+num_posB = 10000;
+w = rand(num_posB,rfpp.num_vertices);
+w = w./bsxfun(@times,sum(w,2),ones(1,rfpp.num_vertices));
+
+yaw = rand()*2*pi;
+posA = randn(2,1);
+pos_vert = bsxfun(@times,ones(1,rfpp.num_vertices),posA)+[cos(yaw) -sin(yaw);sin(yaw) cos(yaw)]*rfpp.vertices;
+posB = pos_vert*w';
+[A,b] = rfpp.halfspace(yaw);
+val = A*[bsxfun(@times,posA,ones(1,num_posB));posB]-bsxfun(@times,b,ones(1,num_posB));
+if(any(any(val>1e-10)))
+  error('Incorrect RelativeFootPositionPolygon');
+end
+end
