9ec730c37dba0f4406a164e7b0ae259d15881023
Hongkai Dai <daihongkai@gmail.com>
2014-05-21 14:16:32 -0400

    dynamics planning takes in polygonal constraint on the relative position between CoM and feet

--- systems/plants/trajOpt/QuasiStaticContact.m
+++ systems/plants/trajOpt/QuasiStaticContact.m
@@ -25,9 +25,9 @@ classdef QuasiStaticContact
       end
     end
     
-    function [Aeq,beq] = halfspace(obj,foot_xy,yaw)
-      % generate the halfspace constraint Aeq*x=beq for x =
-      % [com_x;com_y;weight1;...;weightM]. These
+    function [Ain,bin] = halfspace(obj,foot_xy,yaw)
+      % generate the halfspace constraint Ain*x<=bin for x =
+      % [com_x;com_y]. These
       % linear constraints enforce the CoM lie within the allowable polygon
       % @param foot_xy A 2 x obj.num_fsrc_cnstr matrix. foot_xy(:,i) is the xy location of
       % obj.fsrc_cnstr{i}
@@ -37,15 +37,17 @@ classdef QuasiStaticContact
       contact_pts_count = 0;
       for i = 1:obj.num_fsrc_cnstr
         num_contact_pts_i = obj.fsrc_cnstr{i}.num_contact_pts;
-        [rotmat_fsrc_i,A_fsrc_i,b_fsrc_i] = obj.fsrc_cnstr{i}.fsr_cnstr.bodyTransform(yaw(i));
+        [rotmat_fsrc_i,A_fsrc_i,b_fsrc_i] = obj.fsrc_cnstr{i}.foot_step_region_cnstr.bodyTransform(yaw(i));
         foot_contact_pos(:,contact_pts_count+(1:num_contact_pts_i)) = ...
           bsxfun(@times,ones(1,num_contact_pts_i),A_fsrc_i*foot_xy(:,i)+b_fsrc_i)+rotmat_fsrc_i*obj.fsrc_cnstr{i}.body_contact_pts;
         contact_pts_count = contact_pts_count+num_contact_pts_i;
       end
       center_pt = mean(foot_contact_pos,2);
       support_vert = bsxfun(@times,(1-obj.scale_factor)*ones(1,obj.num_contact_pts),center_pt)+obj.scale_factor*foot_contact_pos;
-      Aeq = [eye(2) -support_vert(1:2,:);zeros(1,2) ones(1,obj.num_contact_pts)];
-      beq = [0;0;1];
+      H = cddmex('hull',struct('V',support_vert(1:2,:)'));
+      Hred = cddmex('reduce_h',H);
+      Ain = Hred.A;
+      bin = Hred.B;
     end
   end
 end
