faf6d934fd00c70f94e0fda448d50d5fe45359dd
Andrés Valenzuela <avalenzu@mit.edu>
2014-06-04 16:06:04 -0400

    Add getCMMdA method for RBM.
    Also adds `testGetCMMdA`.

--- systems/plants/@RigidBodyManipulator/getCMMdA.m
+++ systems/plants/@RigidBodyManipulator/getCMMdA.m
@@ -1,17 +1,12 @@
-function [A,dA] = getCMMdA(model,kinsol,qd)
+function [A,dA] = getCMMdA(model,kinsol)
 % returns the centroidal momentum matrix as described in Orin & Goswami 2008
 %
 % h = A*qd, where h(4:6) is the total linear momentum and h(1:3) is the 
 % total angular momentum in the centroid frame (world fram translated to COM).
 
-if nargout > 1 && nargin < 3
-  error('RigidBodyManipulator:getCMM:NotEnoughArguments',...
-        'If you ask for Adot, you must provide qdot.');
-end
-
 if ~isstruct(kinsol)
   % treat input as getCMM(model,q)
-  kinsol = doKinematics(model,kinsol,false);
+  kinsol = doKinematics(model,kinsol,false,false);
 end
 
 if (kinsol.mex)
@@ -28,19 +23,16 @@ else
   Xworld = cell(m.NB,1); % spatial transforms from world to each body
   Ic = cell(m.NB,1); % body spatial inertias
 
-  if nargout > 1
-    Adot = zeros(6,nq) + 0*q(1);
+  compute_first_derivative = (nargout > 1);
+
+  if compute_first_derivative
+    dA = zeros(6,nq,nq);
     dXup_dq = cell(m.NB,1);
-    dXup = cell(m.NB,1); % dXup_dq * qd
-    dXworld = cell(m.NB,1); % dXworld_dq * qd
+    dXworld_dq = cell(m.NB,1);
     [com, Jcom] = getCOM(model,kinsol);
-    xdot_com = Jcom*qd;
     dXtr = dXtrans(-com); 
     % prob not efficient
-    dXcom = -reshape(dXtr(:,1),[6 6])*xdot_com(1) - ...
-        reshape(dXtr(:,2),[6 6])*xdot_com(2) - ...
-        reshape(dXtr(:,3),[6 6])*xdot_com(3);
-    dXcom_dq = dXtr*Jcom;
+    dXcom_dq = -dXtr*Jcom;
     dIc = cell(m.NB,1); % derivative of body spatial inertias
     dIc_dq = cell(m.NB,1); % derivative of body spatial inertias
   else
@@ -51,7 +43,7 @@ else
   for i = 1:m.NB
     Ic{i} = m.I{i};
     dIc{i} = zeros(6,6);
-    dIc_dq{i} = zeros(36,34);
+    dIc_dq{i} = zeros(36,nq);
   end
   
   for i = m.NB:-1:1
@@ -59,19 +51,20 @@ else
     [Xi,phi] = jcalc(m.pitch(i), q(n));
     Xup{i} = Xi * m.Xtree{i} + 0*q(n);
   
-    if nargout > 1
-      dXidq = djcalc(m.pitch(i), q(n));
-      dXup{i} = dXidq * m.Xtree{i} * qd(n);
-      dXup_dq{i} = dXidq * m.Xtree{i};
+    if compute_first_derivative
+      dXi_dq = zeros(36,nq);
+      dXi_dq(:,n) = reshape(djcalc(m.pitch(i), q(n)),36,1);
+      %dXup_dq{i} = zeros(36,nq);
+      %dXup_dq{i}(:,n) = reshape(dXidq * m.Xtree{i},36,1);
+      dXup_dq{i} = matGradMultMat(Xi,m.Xtree{i},dXi_dq,zeros(36,nq));
     end
     
     Phi{i} = phi;
     if m.parent(i) > 0
       Ic{m.parent(i)} = Ic{m.parent(i)} + Xup{i}'*Ic{i}*Xup{i};
 
-      if nargout > 1
-        dIc{m.parent(i)} = dIc{m.parent(i)} + (dXup{i}'*Ic{i} + Xup{i}'*dIc{i})*Xup{i} + Xup{i}'*Ic{i}*dXup{i};
-        dIc_dq{m.parent(i)} = dIc_dq{m.parent(i)} + (dXup_dq{i}'*Ic{i} + Xup{i}'*dIc_dq{i})*Xup{i} + Xup{i}'*Ic{i}*dXup_dq{i};
+      if compute_first_derivative
+        dIc_dq{m.parent(i)} = dIc_dq{m.parent(i)} + matGradMultMat(Xup{i}',Ic{i}*Xup{i},transposeGrad(dXup_dq{i},[6,6]),matGradMultMat(Ic{i},Xup{i},dIc_dq{i},dXup_dq{i}));
       end
     end
   end
@@ -80,25 +73,27 @@ else
     n = m.dofnum(i);
     if m.parent(i) > 0
       Xworld{i} = Xup{i} * Xworld{m.parent(i)};
-      if nargout > 1
-        dXworld{i} = dXup{i}*Xworld{m.parent(i)} + Xup{i}*dXworld{m.parent(i)};
-        dXworld_dq{i} = dXup_dq{i}*Xworld{m.parent(i)} + Xup{i}*dXworld_dq{m.parent(i)};
+      if compute_first_derivative
+        dXworld_dq{i} = matGradMultMat(Xup{i},Xworld{m.parent(i)},dXup_dq{i},dXworld_dq{m.parent(i)});
       end      
     else
       Xworld{i} = Xup{i};
-      if nargout > 1
-        dXworld{i} = dXup{i};
+      if compute_first_derivative
         dXworld_dq{i} = dXup_dq{i};
       end  
     end
     Xg = Xworld{i} * Xcom; % spatial transform from centroid to body
     A(:,n) = Xg'*Ic{i}*Phi{i};
     
-    if nargout > 1
-      dXg = dXworld{i} * Xcom + Xworld{i} * dXcom; 
-      dXg_dq{i} = dXworld_dq{i} * Xcom + Xworld{i} * dXcom_dq(:,:,i); 
-      Adot(:,n) = dXg'*Ic{i}*Phi{i} + Xg'*dIc{i}*Phi{i};
+    if compute_first_derivative
+      dXg_dq = matGradMultMat(Xworld{i},Xcom,dXworld_dq{i},dXcom_dq); 
+      dA(:,n,:) = matGradMultMat(Xg',Ic{i}*Phi{i},transposeGrad(dXg_dq,size(Xg)),matGradMult(dIc_dq{i},Phi{i}));
     end
+  end
+  %A = Xcom;
+  if compute_first_derivative
+    dA = reshape(dA,6*nq,nq);
+    %dA = dXcom_dq;
   end
 end
 end
