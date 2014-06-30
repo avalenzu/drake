e88ac25e0e956551eb08a65bc7afb5ca9f7d7a20
Hongkai Dai <daihongkai@gmail.com>
2014-06-29 22:19:45 -0400

    1. Add Complementarity contact constraints 2. Change QuadraticSumConstraint to the current formulation of QuadraticConstraint

--- solvers/QuadraticSumConstraint.m
+++ solvers/QuadraticSumConstraint.m
@@ -19,23 +19,33 @@ classdef QuadraticSumConstraint < QuadraticConstraint
       if(size(v,1) ~= Qa_dim)
         error('Drake:QuadraticSumConstraint:Qa and a have inconsistent dimensions');
       end
+      Qa = (Qa+Qa')/2;
       iQ = reshape(repmat(reshape(1:Qa_dim*a_cols,Qa_dim,a_cols),Qa_dim,1),[],1);
       jQ = reshape(bsxfun(@times,(1:Qa_dim*a_cols),ones(Qa_dim,1)),[],1);
       Qval = reshape(bsxfun(@times,Qa(:),ones(1,a_cols)),[],1);
       Q = sparse(iQ,jQ,Qval,a_cols*Qa_dim,a_cols*Qa_dim);
-      obj = obj@QuadraticConstraint(lb,ub,Q,v(:));
+      vQav = sum(sum(v.*(Qa*v)));
+      obj = obj@QuadraticConstraint(lb-vQav,ub-vQav,Q*2,-2*Q*v(:));
       obj.Qa = Qa;
       obj.x_rows = Qa_dim;
       obj.x_cols = a_cols;
       obj.v = v;
     end
-    
-    function [c,dc] = eval(obj,x)
+  end
+  
+  methods (Access = protected)
+    function [c,dc,ddc] = constraintEval(obj,x)
       x = reshape(x,obj.x_rows,obj.x_cols);
       xv = x-obj.v;
       Qxv = obj.Qa*xv;
       c = sum(sum(xv.*Qxv));
-      dc = 2*reshape(Qxv,1,[]);
+      if(nargout>1)
+        dc = 2*reshape(Qxv,1,[]);
+      end
+      if(nargout>2)
+        ddc = reshape(obj.Q,1,[]);
+      end
     end
   end
+  
 end
