d5d8c6d963185b3c28d6af89de96608d707efa67
Andrés Valenzuela <avalenzu@mit.edu>
2015-01-15 12:33:28 -0500

    Use flatter bricks in stackedBricks

--- systems/plants/test/stackedBricks.m
+++ systems/plants/test/stackedBricks.m
@@ -8,16 +8,15 @@ function stackedBricks(N)
   options.ignore_self_collisions = true;
   p = TimeSteppingRigidBodyManipulator([],dt,options);
   for i = 1:N
-    p = p.addRobotFromURDF('FallingBrick.urdf',[],[],options);
+    p = p.addRobotFromURDF('StackingBrick.urdf',[],[],options);
   end
   height = 0.5;
   width = 2;
-  spacing = 1.5;
+  spacing = 0.5;
   x0 = zeros(p.getNumStates(), 1);
   x0(3:6:p.getNumPositions()) = 0.5*height + (height/2:spacing*height:N*spacing*height);
-  x0(1:12:p.getNumPositions()) = 0.2*width;
-  x0(7:12:p.getNumPositions()) = -0.2*width;
-  %x0(4:12:p.getNumPositions()) = pi/2;
+  x0(2:12:p.getNumPositions()) = 0.4*width;
+  x0(8:12:p.getNumPositions()) = -0.4*width;
   x0 = p.resolveConstraints(x0);
   v = p.constructVisualizer();
   v.draw(0,x0);
