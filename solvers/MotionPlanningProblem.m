db303a87966d03e0ea9baac6a46632dda6a70d37
Andrés Valenzuela <avalenzu@mit.edu>
2015-01-29 12:34:26 -0500

    Enhancements to MotionPlanningProblem/rrt
    User can specify interpolation function.
    Constraints can operate over a subset of the search space
    Pass tree as additional output and take it as part of the options struct. This
    allows restarting on an existing tree.

--- solvers/MotionPlanningProblem.m
+++ solvers/MotionPlanningProblem.m
@@ -15,6 +15,8 @@ classdef MotionPlanningProblem
     constraints={};  % a cell array of Constraint objects
     % todo: add differential constraints?  control affine only?
     % todo: add objectives?
+    x_inds={}; % a cell array of indices into the search space. obj.constraints{i}
+               % takes x(obj.x_inds{i}) as its input
   end
 
   methods
@@ -26,7 +28,7 @@ classdef MotionPlanningProblem
       if(nargin<2)
         var_names = cellfun(@(i) sprintf('x%d',i),num2cell((1:obj.num_vars)'),'UniformOutput',false);
       else
-        if(~iscellstr(var_names) || numel(var_names) ~= obj.num_vars)
+        if(~iscellstr(var_names) || numel(var_names) ~= num_vars)
           error('Drake:MotionPlanningProblem:InvalidArgument','Argument x_name should be a cell containing %d strings',obj.num_vars);
         end
         var_names = var_names(:);
@@ -36,7 +38,7 @@ classdef MotionPlanningProblem
 
     end
 
-    function [xtraj,info] = rrt(obj,x_start,x_goal,random_sample_fcn,options)
+    function [xtraj,info,V,parent] = rrt(obj,x_start,x_goal,random_sample_fcn,options)
       % Simple RRT algorithm
       % @param x_start
       % @param x_goal
@@ -54,6 +56,14 @@ classdef MotionPlanningProblem
       % @option max_edge_length a distance by which to trim
       % @options max_length_between_constraint_checks
       % @options goal_bias @default P(try_goal)=.05
+      % @options N maximum number of nodes in the tree @default 10000
+      % @options verbose print additional information @default false
+      %
+      % @retval xtraj search-space trajectory from x_start to x_goal
+      % @retval info exit-flag indicating success (info = 1) or failure (info = 2)
+      % @retval V nX-by-N list of nodes in the tree
+      % @retval parent N-1 list of node indices V(:, parent(i)) is the parent
+      %                node of V(:, i)
 
       % parameters
       sizecheck(x_start,[obj.num_vars,1]);
@@ -64,51 +74,76 @@ classdef MotionPlanningProblem
       if nargin<5, options=struct(); end
       defaultOptions.distance_metric_fcn = @MotionPlanningProblem.euclideanDistance;
       defaultOptions.display_fcn = @MotionPlanningProblem.drawFirstTwoCoordinates;
+      defaultOptions.interpolation_fcn = @MotionPlanningProblem.linearInterpolation;
       defaultOptions.display_after_every = 50;
       defaultOptions.max_edge_length = inf;  % because i don't have any sense of scale here
       defaultOptions.max_length_between_constraint_checks = inf;
       defaultOptions.goal_bias = .05;
+      defaultOptions.V = [];
+      defaultOptions.parent = [];
+      defaultOptions.N = 10000;
+      defaultOptions.verbose = false;
       options = applyDefaults(options,defaultOptions);
       typecheck(options.distance_metric_fcn,'function_handle');
 
-      N = 10000;  % for pre-allocating memory
-      V = repmat(x_start,1,N);  % vertices
-      parent = nan(1,N-1); % edges
+      if isempty(options.V)
+        N = options.N;  % for pre-allocating memory
+        V = repmat(x_start,1,N);  % vertices
+        parent = nan(1,N-1); % edges
 
-      n=2;
-      n_at_last_display=0;
-      info=2; xtraj=[];  % default return values
+        n=2;
+      else
+        n = size(options.V,2)+1;
+        N = n + options.N - 1;  % for pre-allocating memory
+        sizecheck(options.parent,[1,size(options.V,2)-1]);
+        V = [options.V,repmat(x_start,1,N-size(options.V,2))];  % vertices
+        parent = [options.parent,nan(1,N-size(options.V,2)-1)]; % edges
+      end
+      n_at_last_display=1;
+      info = 2; xtraj = [];  % default return values
       while n<=N
+        if options.verbose
+          fprintf('n = %d\n',n);
+        end
         try_goal = rand<options.goal_bias;
         if try_goal
           xs = x_goal;
         else
           xs = random_sample_fcn();
-          if ~checkConstraints(obj,xs), continue; end
         end
 
         d = options.distance_metric_fcn(V(:,1:n-1),xs);
         [dmin,imin] = min(d);
 
-        if dmin>options.max_edge_length
-          % then linearly interpolate along that distance
-          xs = V(:,imin)+(options.max_edge_length/dmin)*(xs - V(:,imin));
-          dmin = options.max_edge_length;
-          try_goal = false;
+        if try_goal && options.verbose
+          fprintf('Min. dist to goal = %f\n',dmin);
         end
 
+        if dmin>options.max_edge_length
+          % then linearly interpolate along that distance
+          [xs,valid_interp] =options.interpolation_fcn(V(:,imin),xs,(options.max_edge_length/dmin));
+          dmin = options.max_edge_length;
+          try_goal = false;
+        else
+          [xs,valid_interp] =options.interpolation_fcn(V(:,imin),xs,1);
+        end
+%         if ~(valid_interp && checkConstraints(obj,xs)), continue; end
+        if ~valid_interp, continue; end
         if dmin>options.max_length_between_constraint_checks
           % then linearly interpolate along that distance
           num_intermediate_samples = 2+ceil(dmin/options.max_length_between_constraint_checks);
-          xss = linspacevec(V(:,imin),xs,num_intermediate_samples);
           valid = true;
+          interp_values = linspace(0,1,num_intermediate_samples);
           for i=2:num_intermediate_samples-1
-            if ~checkConstraints(obj,xss(:,i)),
+            [xss_i,valid_interp] = options.interpolation_fcn(V(:,imin),xs,interp_values(i));
+            if ~(valid_interp && checkConstraints(obj,xss_i))
               valid=false;
               break;
             end
           end
           if ~valid, continue; end
+        else
+          if ~(valid_interp && checkConstraints(obj,xs)), continue; end
         end
 
         V(:,n) = xs;
@@ -132,6 +167,12 @@ classdef MotionPlanningProblem
         if try_goal, return; end
         n=n+1;
       end
+      d = options.distance_metric_fcn(V,x_goal);
+      [~,path] = min(d);
+      while path(1)>1
+        path = [parent(path(1)-1),path];
+      end
+      xtraj = PPTrajectory(foh(1:length(path),V(:,path)));
     end
 
     function obj = addConstraint(obj,constraint,x_inds)
@@ -139,13 +180,14 @@ classdef MotionPlanningProblem
       if nargin<3, x_inds = 1:obj.num_vars; end
       sizecheck(x_inds,constraint.xdim);
 
+      obj.x_inds{end+1} = x_inds;
       obj.constraints = vertcat(obj.constraints,{constraint});
     end
 
     function valid = checkConstraints(obj,x,varargin)
       valid = true;
       for i=1:length(obj.constraints)
-        y = eval(obj.constraints{i},x,varargin{:});
+        y = eval(obj.constraints{i},x(obj.x_inds{i}),varargin{:});
         if any(y<obj.constraints{i}.lb) || any(y>obj.constraints{i}.ub)
           valid = false;
           return;
@@ -161,7 +203,13 @@ classdef MotionPlanningProblem
     end
 
     function drawFirstTwoCoordinates(V,parent,last_drawn_edge_num)
-      line([V(1,(last_drawn_edge_num+2):end);V(1,parent((last_drawn_edge_num+1):end))],[V(2,last_drawn_edge_num+2:end);V(2,parent(last_drawn_edge_num+1:end))],'Color',0.3*[1 1 1]);
+      line([V(1,(last_drawn_edge_num+1):end);V(1,parent((last_drawn_edge_num+0):end))],[V(2,last_drawn_edge_num+1:end);V(2,parent(last_drawn_edge_num+0:end))],'Color',0.3*[1 1 1],'Marker','.');
+      axis equal
+    end
+
+    function [x_interp, valid_interp] = linearInterpolation(x1,x2,alpha)
+      x_interp = x1+alpha*(x2 - x1);
+      valid_interp = true;
     end
   end
 
