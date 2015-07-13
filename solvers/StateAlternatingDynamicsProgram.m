1564d222debd48956eb711f979d562f234e3f7b1
Andres Valenzuela <avalenzu@mit.edu>
2015-07-13 15:57:14 -0400

    WIP

--- solvers/StateAlternatingDynamicsProgram.m
+++ solvers/StateAlternatingDynamicsProgram.m
@@ -3,26 +3,25 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
     I
     m
     N = 2
-    position_max = 1e2
-    velocity_max = 1e2
+    position_max = 1e3
+    velocity_max = 1e3
     force_max = 1e2
     dt = 1e-3
-    r_fixed_array
-    z_fixed_array
-    v_fixed_array
     w_fixed_array
   end
 
   methods
-    function obj = StateAlternatingDynamicsProgram(I, m, N, dt)
+    function obj = StateAlternatingDynamicsProgram(I, m, N, dt, w_fixed_array)
       obj = obj@MixedIntegerConvexProgram(true);
       obj.N = N;
       obj.dt = dt;
       obj.I = I;
       obj.m = m;
+      obj.w_fixed_array = w_fixed_array;
       obj = obj.addPositionVariables();
       obj = obj.addVelocityVariables();
       obj = obj.addDynamicConstraints();
+      obj = obj.addSymbolicCost(sum((obj.vars.w.symb(:) - obj.w_fixed_array(:)).^2));
     end
 
 
@@ -32,13 +31,15 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
       m = obj.m; %#ok
       g = [0; 0; -9.81];
       for n = 1:(obj.N-1)
-        r = obj.vars.(obj.positionName(n)).symb;
-        z = obj.vars.(obj.orientationName(n)).symb;
-        w = obj.vars.(obj.angularVelocityName(n)).symb;
+        r = obj.vars.r.symb(:, n);
+        z = obj.vars.z.symb(:, n);
+        v = obj.vars.v.symb(:, n);
+        w = obj.vars.w.symb(:, n);
 
-        r_next = obj.vars.(obj.positionName(n+1)).symb;
-        z_next = obj.vars.(obj.orientationName(n+1)).symb;
-        w_next = obj.vars.(obj.angularVelocityName(n+1)).symb;
+        r_next = obj.vars.r.symb(:, n+1);
+        z_next = obj.vars.z.symb(:, n+1);
+        w_next = obj.vars.w.symb(:, n+1);
+        v_next = obj.vars.v.symb(:, n+1);
         Y_next = I*w_next; %#ok
 
         w_fixed = obj.w_fixed_array(:, n);
@@ -46,13 +47,13 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
         w_mid_fixed = 0.5*(w_fixed + w_next_fixed);
         A = expmap2quat(-h/2*w_mid_fixed);
         B = expmap2quat(h/2*w_mid_fixed);
-        T = 0;
-        T_next = 0;
+        T = zeros(3, 1);
+        T_next = zeros(3, 1);
         F = m*g; %#ok
         F_next = m*g; %#ok
-        Y_next_desired = quatRotateVector(A, ...
-                            quatRotateVector(A, I*w + h/2*T) ...
-                            + h/2*quatRotateVector(B, T_next)); %#ok
+        Y_next_desired = quatRotateVec(A, ...
+                            quatRotateVec(A, I*w + h/2*T) ...
+                            + h/2*quatRotateVec(B, T_next)); %#ok
         z_next_desired = quatProduct(z, expmap2quat(h*w_mid_fixed));
         v_mid = v + h/(2*m)*F; %#ok
         r_next_desired = r + h*v_mid;
@@ -63,42 +64,28 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
         obj = obj.addSymbolicConstraints(Y_next == Y_next_desired);
       end
     end
-
+    
+    function obj = addPositionConstraint(obj, time_index, lb, ub)
+      r = obj.vars.r.symb(:, time_index);
+      obj = obj.addSymbolicConstraints([lb <= r <= ub]); %#ok
+    end
+    
+    function obj = addOrientationConstraint(obj, time_index, val)
+      z = obj.vars.z.symb(:, time_index);
+      obj = obj.addSymbolicConstraints(z == val);
+    end
+    
     function obj = addPositionVariables(obj)
-      for n = 1:obj.N
-        obj = obj.addVariable(obj.positionName(n), 'C', ...
-          [3,1], -obj.position_max, ...
-          obj.position_max);
-        obj = obj.addVariable(obj.orientationName(n), 'C', [4,1], 0, 1);
-      end
+      obj = obj.addVariable('r', 'C', ...
+        [3, obj.N], -obj.position_max, obj.position_max);
+      obj = obj.addVariable('z', 'C', [4, obj.N], 0, 1);
     end
 
     function obj = addVelocityVariables(obj)
-        for n = 1:obj.N
-          obj = obj.addVariable(obj.velocityName(n), 'C', ...
-                                [3,1], -obj.velocity_max, obj.velocity_max);
-          obj = obj.addVariable(obj.angularVelocityName(n), 'C', ...
-                                [3,1], -obj.velocity_max, obj.velocity_max);
-        end
+      obj = obj.addVariable('v', 'C', ...
+        [3,obj.N], -obj.velocity_max, obj.velocity_max);
+      obj = obj.addVariable('w', 'C', ...
+        [3,obj.N], -obj.velocity_max, obj.velocity_max);
     end
-  end
-
-  methods (Static)
-    function name = positionName(time_index)
-      name = sprintf('r%d', time_index);
-    end
-
-    function name = orientationName(time_index)
-      name = sprintf('z%d', time_index);
-    end
-
-    function name = velocityName(time_index)
-      name = sprintf('v%d', time_index);
-    end
-
-    function name = angularVelocityName(time_index)
-      name = sprintf('w%d', time_index);
-    end
-
   end
 end
