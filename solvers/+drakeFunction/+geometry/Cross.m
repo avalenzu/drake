7e2cb9af7ca9da4975ae92ee6c97443637aa58ad
Andres Valenzuela <avalenzu@mit.edu>
2015-08-03 09:59:13 -0400

    Fix drakeFunction cross products

--- solvers/+drakeFunction/+geometry/Cross.m
+++ solvers/+drakeFunction/+geometry/Cross.m
@@ -6,9 +6,14 @@ classdef Cross < drakeFunction.DrakeFunction
       obj = obj@drakeFunction.DrakeFunction(dim_input, dim_output);
     end
 
-    function [f, df] = eval(x)
+    function [f, df] = eval(~, x)
       f = cross(x(1:3), x(4:6));
-      df = [-vectorToSkewSymmetric(x(1:3)), vectorToSkewSymmetric(x(4:6))];
+      df = [-vectorToSkewSymmetric(x(4:6)), vectorToSkewSymmetric(x(1:3))];
+    end
+    
+    function obj = setSparsityPattern(obj)
+      [~, df] = obj.eval(ones(6,1));
+      [obj.iCfun, obj.jCvar] = find(df);
     end
   end
 end
