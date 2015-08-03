17b8b60d5e1320c59efe39c969e42ff5bf6f83c3
Andres Valenzuela <avalenzu@mit.edu>
2015-08-03 10:00:19 -0400

    Fix drakeFunction.ScalarProduct

--- solvers/+drakeFunction/ScalarProduct.m
+++ solvers/+drakeFunction/ScalarProduct.m
@@ -1,18 +1,18 @@
 classdef ScalarProduct < drakeFunction.DrakeFunction
   properties (SetAccess = immutable)
-    dim_vector
+    vector_indices
   end
   methods
     function obj = ScalarProduct(dim_vector)
       dim_input = 1 + dim_vector;
       dim_output = dim_vector;
       obj = obj@drakeFunction.DrakeFunction(dim_input, dim_output);
-      obj.dim_vector = dim_vector;
+      obj.vector_indices = 2:(dim_vector + 1);
     end
 
     function [f, df] = eval(obj, x)
-      f = x(1)*x(1:obj.dim_vector);
-      df = [x(1:obj.dim_vector); x(1)*eye(obj.dim_vector)];
+      f = x(1)*x(obj.vector_indices);
+      df = [x(obj.vector_indices), x(1)*eye(numel(obj.vector_indices))];
     end
   end
 end
