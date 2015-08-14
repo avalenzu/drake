e058c9bfcf373a20998c17e286861770c81714d2
Andrés Valenzuela <avalenzu@mit.edu>
2015-08-14 11:27:56 -0400

    More tweaks

--- drake/solvers/test/HopperVisualizer.m
+++ drake/solvers/test/HopperVisualizer.m
@@ -2,7 +2,6 @@ classdef HopperVisualizer < Visualizer
   properties
     v
     Ftraj
-    rHipTraj
     lcmgl
     r_foot_inds = 7:9;
   end
@@ -13,9 +12,8 @@ classdef HopperVisualizer < Visualizer
       obj.lcmgl = LCMGLClient();
     end
 
-    function playback(obj, qtraj, Ftraj, rHipTraj, varargin)
+    function playback(obj, qtraj, Ftraj, varargin)
       obj.Ftraj = Ftraj;
-      obj.rHipTraj = rHipTraj;
       playback@Visualizer(obj, qtraj, varargin{:});
     end
 
@@ -26,10 +24,6 @@ classdef HopperVisualizer < Visualizer
       r_foot = q(obj.r_foot_inds);
       pt = r_foot + F;
       obj.lcmgl.line3(r_foot(1), r_foot(2) ,r_foot(3), pt(1), pt(2), pt(3));
-      r_hip = eval(obj.rHipTraj, t);
-      r_hip = q(1:3) + [r_hip(1); 0; r_hip(2)];
-      obj.lcmgl.glColor3f(1, 0, 0);
-      obj.lcmgl.sphere(r_hip, 0.01, 20, 20);
       obj.lcmgl.switchBuffers();
       obj.v.draw(t, q);
     end
