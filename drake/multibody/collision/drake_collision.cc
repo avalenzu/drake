58b1bb897fa737e1c0cb7879a28583a4e7797232
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-07-11 15:30:55 -0400

    Let bullet_collision and fcl_collision coexist
    FCL is the default though. And there's no way to choose to use bullet :)

--- drake/multibody/collision/drake_collision.cc
+++ drake/multibody/collision/drake_collision.cc
@@ -2,14 +2,22 @@
 
 #include "drake/multibody/collision/collision_filter.h"
 
+#ifdef BULLET_COLLISION
+#include "drake/multibody/collision/bullet_model.h"
+#else
 #include "drake/multibody/collision/fcl_model.h"
+#endif
 
 using std::unique_ptr;
 
 namespace DrakeCollision {
 
 unique_ptr<Model> newModel() {
+#ifdef BULLET_COLLISION
+  return unique_ptr<Model>(new BulletModel());
+#else
   return unique_ptr<Model>(new FCLModel());
+#endif
 }
 
 }  // namespace DrakeCollision
