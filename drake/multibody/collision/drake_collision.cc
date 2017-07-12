5d2f145c761d3a1b31428d0d4ba4580f9daf6cfc
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-07-12 07:56:24 -0400

    Add model type arg to DrakeCollision::newModel

--- drake/multibody/collision/drake_collision.cc
+++ drake/multibody/collision/drake_collision.cc
@@ -4,20 +4,43 @@
 
 #ifdef BULLET_COLLISION
 #include "drake/multibody/collision/bullet_model.h"
-#else
-#include "drake/multibody/collision/unusable_model.h"
 #endif
+
+#include "drake/common/drake_assert.h"
+#include "drake/multibody/collision/unusable_model.h"
+#include "drake/multibody/collision/fcl_model.h"
 
 using std::unique_ptr;
 
 namespace DrakeCollision {
 
-unique_ptr<Model> newModel() {
+enum ModelType {
+  kUnusable = 0,
+  kFcl = 1,
 #ifdef BULLET_COLLISION
-  return unique_ptr<Model>(new BulletModel());
-#else
-  return unique_ptr<Model>(new UnusableModel());
+  kBullet = 2
 #endif
+};
+
+unique_ptr<Model> newModel(ModelType type) {
+  switch (type) {
+    case (kUnusable): {
+      return unique_ptr<Model>(new UnusableModel());
+      break;
+    }
+    case (kFcl): {
+      return unique_ptr<Model>(new FCLModel());
+      break;
+    }
+#ifdef BULLET_COLLISION
+    case kBullet: {
+      return unique_ptr<Model>(new BulletModel());
+      break;
+    }
+#endif
+    default:
+      DRAKE_ABORT_MSG("Unexpected collision model type.");
+  }
 }
 
 }  // namespace DrakeCollision
