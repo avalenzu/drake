2be8643618d3d1dcf8fb2e0c4ee3aba01b1a83db
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-09-05 08:56:25 -0400

    Fill in KinematicTrajectoryOptimization

--- drake/manipulation/planner/kinematic_trajectory_optimization.cc
+++ drake/manipulation/planner/kinematic_trajectory_optimization.cc
@@ -1,14 +1,53 @@
 #include "drake/manipulation/planner/kinematic_trajectory_optimization.h"
 
-using drake::multibody::MultibodyTree;
+#include "drake/systems/trajectory_optimization/direct_transcription.h"
+
+using Eigen::MatrixXd;
+using drake::systems::LinearSystem;
+using drake::systems::trajectory_optimization::DirectTranscription;
+using drake::solvers::SolutionResult;
 
 namespace drake {
 namespace manipulation {
 namespace planner {
 
 KinematicTrajectoryOptimization::KinematicTrajectoryOptimization(
-    const MultibodyTree<double>& tree, int num_time_samples){};
+    const RigidBodyTree<double>& tree, int num_time_samples)
+    : tree_(tree.Clone()),
+      num_time_samples_(num_time_samples) {
+  const int kNumStates{3 * num_positions()};
+  const int kNumInputs{num_positions()};
+  const int kNumOutputs{0};
 
-}  // namespace planner
+  const double dt{0.1};
+  const MatrixXd kZero{MatrixXd::Zero(num_positions(), num_positions())};
+  const MatrixXd kIdentity{
+      MatrixXd::Identity(num_positions(), num_positions())};
+
+  MatrixX<double> A{kNumStates, kNumStates};
+  A << kIdentity, dt*kIdentity, kZero, kZero, kIdentity, dt*kIdentity, kZero, kZero, kIdentity;
+
+  MatrixXd B{kNumStates, kNumInputs};
+  B << kZero, kZero, dt*kIdentity;
+
+  MatrixXd C{kNumOutputs, kNumStates};
+  MatrixXd D{kNumOutputs, kNumInputs};
+
+  system_ = std::make_unique<LinearSystem<double>>(A, B, C, D, dt);
+
+  prog_ = std::make_unique<DirectTranscription>(
+      system_.get(), *(system_->CreateDefaultContext()), num_time_samples_);
+};
+
+void KinematicTrajectoryOptimization::AddRunningCost(
+    const symbolic::Expression& g) {
+  prog_->AddRunningCost(g);
+};
+
+SolutionResult KinematicTrajectoryOptimization::Solve() {
+    return prog_->Solve();
+};
+
+}  // }amespace planner
 }  // namespace manipulation
 }  // namespace drake
--- drake/manipulation/planner/test/kinematic_trajectory_optimization_test.cc
+++ drake/manipulation/planner/test/kinematic_trajectory_optimization_test.cc
@@ -1,20 +1,66 @@
 #include "drake/manipulation/planner/kinematic_trajectory_optimization.h"
-#include "drake/multibody/multibody_tree/multibody_tree.h"
 
 #include <gtest/gtest.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/multibody/parsers/urdf_parser.h"
+#include "drake/multibody/rigid_body_tree.h"
+
+using drake::systems::trajectory_optimization::MultipleShooting;
 
 namespace drake {
 namespace manipulation {
 namespace planner {
 namespace {
 
-using drake::multibody::MultibodyTree;
-
 GTEST_TEST(KinematicTrajectoryOptimizationTest, ConstructorTest) {
   // Test that the constructor works and that the expected ports are
   // present.
-  MultibodyTree<double> tree;
-  EXPECT_NO_THROW(KinematicTrajectoryOptimization(tree, 0));
+  const std::string kModelPath = FindResourceOrThrow(
+      "drake/manipulation/models/iiwa_description/urdf/"
+      "iiwa14_polytope_collision.urdf");
+  RigidBodyTree<double> tree{};
+  drake::parsers::urdf::AddModelInstanceFromUrdfFile(kModelPath, multibody::joints::kFixed, nullptr, &tree);
+  const int kNumSampleTimes{2};
+
+  KinematicTrajectoryOptimization prog{tree, kNumSampleTimes};
+  EXPECT_EQ(prog.num_time_samples(), kNumSampleTimes);
+}
+
+GTEST_TEST(KinematicTrajectoryOptimizationTest, SolveTest) {
+  // Test that the constructor works and that the expected ports are
+  // present.
+  const std::string kModelPath = FindResourceOrThrow(
+      "drake/manipulation/models/iiwa_description/urdf/"
+      "iiwa14_polytope_collision.urdf");
+  RigidBodyTree<double> tree{};
+  drake::parsers::urdf::AddModelInstanceFromUrdfFile(kModelPath, multibody::joints::kFixed, nullptr, &tree);
+  const int kNumSampleTimes{2};
+
+  KinematicTrajectoryOptimization kin_traj_opt{tree, kNumSampleTimes};
+
+  MultipleShooting* prog = kin_traj_opt.mutable_prog();
+
+  // x[0] = 0.
+  VectorX<double> x0{VectorX<double>::Zero(prog->initial_state().size())};
+  prog->AddLinearConstraint(prog->initial_state() == x0);
+
+  const solvers::VectorXDecisionVariable& u = prog->input();
+  prog->AddRunningCost( u.transpose() * u );
+
+  solvers::SolutionResult result{prog->Solve()};
+
+  EXPECT_EQ(result, solvers::kSolutionFound);
+  for (int i = 0; i < (kNumSampleTimes - 1); ++i) {
+    for (int j = 0; j < prog->state().size(); ++j) {
+      EXPECT_EQ(prog->GetSolution(prog->state(i)[j]), 0.0);
+    }
+    for (int j = 0; j < prog->input().size(); ++j) {
+      EXPECT_EQ(prog->GetSolution(prog->input(i)[j]), 0.0);
+    }
+  }
+
+
 }
 }  // namespace
 }  // namespace planner
