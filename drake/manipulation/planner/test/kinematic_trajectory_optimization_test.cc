1dd86ad47fac6468e1428a93fef6120001fadd1a
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-10-02 16:51:04 -0400

    [WIP]

--- drake/manipulation/planner/test/kinematic_trajectory_optimization_test.cc
+++ drake/manipulation/planner/test/kinematic_trajectory_optimization_test.cc
@@ -1,10 +1,12 @@
 #include "drake/manipulation/planner/kinematic_trajectory_optimization.h"
 
 #include <memory>
+#include <string>
 
 #include <gtest/gtest.h>
 
 #include "drake/common/find_resource.h"
+#include "drake/common/proto/call_matlab.h"
 #include "drake/manipulation/planner/kinematic_planning_problem.h"
 #include "drake/multibody/parsers/urdf_parser.h"
 #include "drake/multibody/rigid_body_tree.h"
@@ -13,6 +15,8 @@ namespace drake {
 namespace manipulation {
 namespace planner {
 namespace {
+
+using common::CallMatlab;
 
 const char* const kIiwaUrdf =
     "drake/manipulation/models/iiwa_description/urdf/"
@@ -34,20 +38,35 @@ class FromKinematicPlanningProblemTest : public ::testing::Test {
 
 GTEST_TEST(KinematicTrajectoryOptimizationTest, UnconstrainedTest) {
   KinematicTrajectoryOptimization program{
-      1 /*num_positions*/, 2 /*num_control_points*/,
+      1 /*num_positions*/, 4 /*num_control_points*/,
       -1 /*num_evaluation_points*/, 4 /*spine_order*/};
-  program.AddLinearConstraint(program.control_points().leftCols(1)(0) == 0);
-  program.AddLinearConstraint(program.control_points().rightCols(1)(0) == 1);
+  program.AddLinearConstraint(program.position(0)(0) == 0);
+  program.AddLinearConstraint(program.position(1)(0) == 3);
   solvers::SolutionResult result = program.Solve();
   EXPECT_EQ(result, solvers::kSolutionFound);
+
+  const int kNumPlottingPoints(1000);
+  PiecewisePolynomialTrajectory solution_trajectory{
+      program.ReconstructPositionTrajectory()};
+  const VectorX<double> x{VectorX<double>::LinSpaced(
+      kNumPlottingPoints, solution_trajectory.get_start_time(),
+      solution_trajectory.get_end_time())};
+  VectorX<double> solution_values(x.size());
+  for (int i = 0; i < kNumPlottingPoints; ++i) {
+    solution_values(i) = solution_trajectory.value(x(i))(0);
+  }
+  CallMatlab("plot", x, solution_values, "LineWidth", 2.0);
+  std::string plot_title("Solution Result:" + std::to_string(result) +
+                         ", Solver: " + program.GetSolverId()->name());
+  CallMatlab("title", plot_title);
 }
 
-//TEST_F(FromKinematicPlanningProblemTest, UnconstrainedTest) {
-  //KinematicTrajectoryOptimization program{
-      //problem_.get(), 2 [>num_control_points*/, -1 /*num_evaluation_points<],
-      //4 [>spline_order<]};
-  //solvers::SolutionResult result = program.Solve();
-  //EXPECT_EQ(result, solvers::kSolutionFound);
+// TEST_F(FromKinematicPlanningProblemTest, UnconstrainedTest) {
+// KinematicTrajectoryOptimization program{
+// problem_.get(), 2 [>num_control_points*/, -1 /*num_evaluation_points<],
+// 4 [>spline_order<]};
+// solvers::SolutionResult result = program.Solve();
+// EXPECT_EQ(result, solvers::kSolutionFound);
 //}
 
 }  // namespace
