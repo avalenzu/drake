4d3569b7fe6070f1b1ac3c141e58d6f1f10c0d16
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-10-02 09:36:16 -0400

    [WIP] Add test.

--- drake/manipulation/planner/kinematic_trajectory_optimization.h
+++ drake/manipulation/planner/kinematic_trajectory_optimization.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+
+#include "drake/common/trajectories/piecewise_polynomial_trajectory.h"
 #include "drake/manipulation/planner/kinematic_planning_problem.h"
 #include "drake/solvers/mathematical_program.h"
 
@@ -14,14 +17,37 @@ namespace planner {
 class KinematicTrajectoryOptimization : public solvers::MathematicalProgram {
  public:
   /**
-   * Constructs a
+   * Constructs a mathematical program whose decision variables are the control
+   * points of a @p spline_order B-form spline. Constraints are enforced at @p
+   * num_evaluation_points evenly spaced points along the trajectory, as well as
+   * points corresponding to their start and end times.
    */
-  KinematicTrajectoryOptimization(const KinematicPlanningProblem& problem,
+  KinematicTrajectoryOptimization(int num_positions, int num_control_points,
+                                  int num_evaluation_points = -1,
+                                  int spline_order = 4);
+
+  /**
+   * Constructs a mathematical program whose decision variables are the control
+   * points of a @p spline_order B-form spline. Constraints are enforced at @p
+   * num_evaluation_points evenly spaced points along the trajectory, as well as
+   * points corresponding to their start and end times.
+   */
+  KinematicTrajectoryOptimization(const KinematicPlanningProblem* problem,
                                   int num_control_points,
                                   int num_evaluation_points = -1,
                                   int spline_order = 4);
 
+  const solvers::MatrixXDecisionVariable& control_points() const {
+    return control_points_;
+  }
+
+  virtual PiecewisePolynomialTrajectory ReconstructPositionTrajectory()
+      const = 0;
+
  private:
+  void AddCost(const solvers::Binding<solvers::Cost>& cost,
+               const Vector2<double> plan_interval);
+  const KinematicPlanningProblem* problem_;
   const int kNumControlPoints_;
   const int kNumEvaluationPoints_;
   const int kOrder_;
@@ -29,6 +55,9 @@ class KinematicTrajectoryOptimization : public solvers::MathematicalProgram {
   const int kNumPositions_;
 
   const solvers::MatrixXDecisionVariable control_points_;
+
+  std::vector<double> knots_;
+  std::vector<PiecewisePolynomial<double>> basis_;
 };
 
 }  // namespace planner
