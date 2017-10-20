c900ec8784d0b07969a799e85f82d34f7d6ae012
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-10-20 08:18:28 -0400

    Use LcmPlanInterpolator in iiwa_controller.

--- drake/examples/kuka_iiwa_arm/iiwa_controller.cc
+++ drake/examples/kuka_iiwa_arm/iiwa_controller.cc
@@ -11,10 +11,10 @@
 #include "drake/common/text_logging.h"
 #include "drake/common/text_logging_gflags.h"
 #include "drake/examples/kuka_iiwa_arm/iiwa_lcm.h"
+#include "drake/examples/kuka_iiwa_arm/lcm_plan_interpolator.h"
 #include "drake/lcm/drake_lcm.h"
 #include "drake/lcmt_iiwa_command.hpp"
 #include "drake/lcmt_iiwa_status.hpp"
-#include "drake/manipulation/planner/robot_plan_interpolator.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/context.h"
 #include "drake/systems/framework/diagram.h"
@@ -54,71 +54,57 @@ int DoMain() {
   drake::log()->debug("Status channel: {}", kLcmStatusChannel);
   drake::log()->debug("Command channel: {}", kLcmCommandChannel);
   drake::log()->debug("Plan channel: {}", kLcmPlanChannel);
-  auto plan_sub =
-      builder.AddSystem(systems::lcm::LcmSubscriberSystem::Make<robot_plan_t>(
-          kLcmPlanChannel, &lcm));
-  plan_sub->set_name("plan_sub");
 
   const std::string urdf =
       (!FLAGS_urdf.empty() ? FLAGS_urdf : FindResourceOrThrow(kIiwaUrdf));
 
   // Sets the robot plan interpolation type.
-  RobotPlanInterpolator* plan_source;
   std::string interp_str(FLAGS_interp_type);
   std::transform(interp_str.begin(), interp_str.end(),
                  interp_str.begin(), ::tolower);
+  InterpolatorType interpolator_type{};
   if (interp_str == "zoh") {
-    plan_source = builder.AddSystem<RobotPlanInterpolator>(
-        urdf, InterpolatorType::ZeroOrderHold);
+    interpolator_type = InterpolatorType::ZeroOrderHold;
   } else if (interp_str == "foh") {
-    plan_source = builder.AddSystem<RobotPlanInterpolator>(
-        urdf, InterpolatorType::FirstOrderHold);
+    interpolator_type = InterpolatorType::FirstOrderHold;
   } else if (interp_str == "cubic") {
-    plan_source = builder.AddSystem<RobotPlanInterpolator>(
-        urdf, InterpolatorType::Cubic);
+    interpolator_type = InterpolatorType::Cubic;
   } else if (interp_str == "pchip") {
-    plan_source = builder.AddSystem<RobotPlanInterpolator>(
-        urdf, InterpolatorType::Pchip);
+    interpolator_type = InterpolatorType::Pchip;
   } else {
-    DRAKE_ABORT_MSG("Robot plan interpolation type not recognized. "
-                    "Use the gflag --helpshort to display "
-                    "flag options for interpolator type.");
+    DRAKE_ABORT_MSG(
+        "Robot plan interpolation type not recognized. "
+        "Use the gflag --helpshort to display "
+        "flag options for interpolator type.");
   }
-  plan_source->set_name("plan_source");
-  const int num_joints = plan_source->tree().get_num_positions();
+  auto plan_interpolator = builder.AddSystem<LcmPlanInterpolator>(urdf, interpolator_type);
+  const int kNumJoints = plan_interpolator->num_joints();
+
+  auto plan_sub =
+      builder.AddSystem(systems::lcm::LcmSubscriberSystem::Make<robot_plan_t>(
+          kLcmPlanChannel, &lcm));
+  plan_sub->set_name("plan_sub");
 
   auto status_sub = builder.AddSystem(
       systems::lcm::LcmSubscriberSystem::Make<lcmt_iiwa_status>(
           kLcmStatusChannel, &lcm));
   status_sub->set_name("status_sub");
 
-  auto status_receiver = builder.AddSystem<IiwaStatusReceiver>(num_joints);
-  status_receiver->set_name("status_receiver");
-
-  auto target_demux =
-      builder.AddSystem<systems::Demultiplexer>(num_joints * 2, num_joints);
-  target_demux->set_name("target_demux");
-
   auto command_pub = builder.AddSystem(
       systems::lcm::LcmPublisherSystem::Make<lcmt_iiwa_command>(
           kLcmCommandChannel, &lcm));
   command_pub->set_name("command_pub");
 
-  auto command_sender = builder.AddSystem<IiwaCommandSender>(num_joints);
-  command_sender->set_name("command_sender");
-
+  // Connect subscribers to input ports.
   builder.Connect(plan_sub->get_output_port(0),
-                  plan_source->get_plan_input_port());
+      plan_interpolator->get_input_port_iiwa_plan());
   builder.Connect(status_sub->get_output_port(0),
-                  status_receiver->get_input_port(0));
-  builder.Connect(status_receiver->get_measured_position_output_port(),
-                  plan_source->get_state_input_port());
-  builder.Connect(plan_source->get_output_port(0),
-                  target_demux->get_input_port(0));
-  builder.Connect(target_demux->get_output_port(0),
-                  command_sender->get_input_port(0));
-  builder.Connect(command_sender->get_output_port(0),
+                  plan_interpolator->get_input_port_iiwa_status());
+
+  // Connect publisher to output port.
+  builder.Connect(plan_interpolator->get_output_port_iiwa_command(),
                   command_pub->get_input_port(0));
+
   auto diagram = builder.Build();
 
   drake::log()->info("controller started");
@@ -133,9 +119,9 @@ int DoMain() {
   double msg_time =
       loop.get_message_to_time_converter().GetTimeInSeconds(first_msg);
   const lcmt_iiwa_status& first_status = first_msg.GetValue<lcmt_iiwa_status>();
-  VectorX<double> q0(num_joints);
-  DRAKE_DEMAND(num_joints == first_status.num_joints);
-  for (int i = 0; i < num_joints; i++)
+  VectorX<double> q0(kNumJoints);
+  DRAKE_DEMAND(kNumJoints == first_status.num_joints);
+  for (int i = 0; i < kNumJoints; i++)
     q0[i] = first_status.joint_position_measured[i];
 
   systems::Context<double>* diagram_context = loop.get_mutable_context();
@@ -145,10 +131,9 @@ int DoMain() {
 
   // Explicit initialization.
   diagram_context->set_time(msg_time);
-  auto& plan_source_context =
-      diagram->GetMutableSubsystemContext(*plan_source, diagram_context);
-  plan_source->Initialize(msg_time, q0,
-                          plan_source_context.get_mutable_state());
+  auto& plan_interpolator_context =
+      diagram->GetMutableSubsystemContext(*plan_interpolator, diagram_context);
+  plan_interpolator->Initialize(msg_time, q0, &plan_interpolator_context);
 
   loop.RunToSecondsAssumingInitialized();
   return 0;
