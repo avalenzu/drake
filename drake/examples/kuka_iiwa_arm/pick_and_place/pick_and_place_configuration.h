b623b90445e739b206b2f9ce70e90027490930d2
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-10-20 22:04:59 -0400

    Add pick_and_place_configuration and protobuf parsing.

---
+++ drake/examples/kuka_iiwa_arm/pick_and_place/pick_and_place_configuration.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "drake/common/eigen_types.h"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+namespace pick_and_place {
+
+struct OptitrackInfo {
+  int id{0};
+  Isometry3<double> X_MF{Isometry3<double>::Identity()};
+};
+
+struct PlannerConfiguration {
+  std::string model_path;
+  std::string end_effector_name;
+  int iiwa_base_optitrack_id{};
+  int target_optitrack_id{};
+  Vector3<double> target_dimensions;
+  std::vector<OptitrackInfo> table_optitrack_info;
+  std::vector<double> table_radii;
+  double period_sec{0.01};
+};
+
+struct SimulatedPlantConfiguration {
+  std::vector<Isometry3<double>> robot_base_poses;
+  std::vector<std::string> table_models;
+  std::vector<Isometry3<double>> table_poses;
+  std::vector<std::string> object_models;
+  std::vector<Isometry3<double>> object_poses;
+  double static_friction_coef{0.9};
+  double dynamic_friction_coef{0.5};
+  double v_stiction_tolerance{0.01};
+  double stiffness{10000};
+  double dissipation{2};
+};
+
+struct OptitrackConfiguration {
+  std::vector<int> robot_base_optitrack_ids{};
+  std::vector<int> object_optitrack_ids{};
+  std::vector<OptitrackInfo> table_optitrack_info;
+};
+
+}  // namespace pick_and_place
+}  // namespace kuka_iiwa_arm
+}  // namespace examples
+}  // namespace drake
