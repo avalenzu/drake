b7fb4744411c0b5051b57f8bef80c18a174becb7
Andrés Valenzuela <andres.valenzuela@tri.global>
2018-01-19 15:26:01 -0500

    [WIP] Start populating ports.

--- manipulation/planner/differential_inverse_kinematics_system.cc
+++ manipulation/planner/differential_inverse_kinematics_system.cc
@@ -4,15 +4,28 @@ namespace drake {
 namespace manipulation {
 namespace planner {
 
+using systems::BasicVector;
+using systems::kVectorValued;
 using systems::Parameters;
+
 DifferentialInverseKinematicsSystem::DifferentialInverseKinematicsSystem(
-      std::unique_ptr<RigidBodyTree<double>> robot,
-      const std::string& end_effector_frame_name) {
-}
-
-/// Reserves the parameters as required by CreateDefaultContext.
-virtual std::unique_ptr<Parameters<double>> AllocateParameters() const {
-
+    std::unique_ptr<RigidBodyTree<double>> robot,
+    const std::string& end_effector_frame_name) {
+  const int num_positions = robot->get_num_positions();
+  const int num_velocities = robot->get_num_velocities();
+  // Input ports
+  joint_position_input_port_ =
+      DeclareInputPort(kVectorValued, num_positions).get_index();
+  joint_velocity_input_port_ =
+      DeclareInputPort(kVectorValued, num_velocities).get_index();
+  desired_end_effector_velocity_input_port_ =
+      DeclareInputPort(kVectorValued, 6).get_index();
+  // Ouput ports
+  desired_joint_velocity_output_port_ =
+      DeclareVectorOutputPort(
+          BasicVector<double>(num_velocities),
+          &DifferentialInverseKinematicsSystem::CalcDesiredJointVelocityOutput)
+          .get_index();
 }
 
 }  // namespace planner
